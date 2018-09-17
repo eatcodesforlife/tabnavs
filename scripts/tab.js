e7f5be5adeaed76902701bd4c3b8ce15bf1e3052
Cris Acosta <iamcris87@gmail.com>
2018-09-17 22:17:59 +0800

    added closable tabs section: html,css & js

--- scripts/tab.js
+++ scripts/tab.js
@@ -1,7 +1,16 @@
-function openContent(pages){
+//basic tab function
+function openContent(tabpages){
   var page = document.getElementsByClassName('content');
   for (i = 0; i < page.length; i++) {
     page[i].style.display = "none";
   }
-  document.getElementById(pages).style.display = 'flex';
+  document.getElementById(tabpages).style.display = 'flex';
+}
+
+function closableTabs(tabpages){
+  var page = document.getElementsByClassName('closableContent');
+  for (i = 0; i < page.length; i++) {
+    page[i].style.display = "none";
+  }
+  document.getElementById(tabpages).style.display = 'flex';
 }
