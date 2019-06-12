37cdef64c9b88db2560245f36eee2adf54e28d05
Cris Acosta <iamcris87@gmail.com>
2019-06-12 11:29:34 +0800

    refactored basic tab

--- scripts/tab.js
+++ scripts/tab.js
@@ -1,33 +1,46 @@
-//basic tab function
-function openContent(tabpages){
-  var page = document.getElementsByClassName('content');
-  for (i = 0; i < page.length; i++) {
-    page[i].style.display = "none";
-  }
-  document.getElementById(tabpages).style.display = 'flex';
+// basic tab function
+
+let myPage = document.querySelectorAll(".content");
+let tabs = document.querySelectorAll(".btn");
+
+// add event lister to each tab
+// for (i = 0; i < tabs.length; i++) {
+//   tabs[i].addEventListener("click", clickTab);
+// }
+
+tabs.forEach(tab => tab.addEventListener("click", clickTab)); //use querySelector to use this
+
+function clickTab(e) {
+  let tabID = e.currentTarget.id;
+  let pageID = tabID.replace("t", "c");
+
+  // hide pages
+  myPage.forEach(page => page.classList.remove("active"));
+
+  let displayedPage = document.querySelector("#" + pageID);
+  displayedPage.classList.add("active");
 }
 
 // closable tab function
-function closableTabs(tabpages){
-  var page = document.getElementsByClassName('closableContent');
+function closableTabs(tabpages) {
+  var page = document.getElementsByClassName("closableContent");
   for (i = 0; i < page.length; i++) {
     page[i].style.display = "none";
   }
-  document.getElementById(tabpages).style.display = 'flex';
+  document.getElementById(tabpages).style.display = "flex";
 }
 
 // active/current tab function
 
 function activeTab(evnt, highlighted) {
-  var currenttab;
-  var pages = document.getElementsByClassName('selectedPage');
-  for (i = 0; i < pages.length; i++) {
-    pages[i].style.display = "none";
-  }
+  document
+    .querySelectorAll(".selectedPage")
+    .forEach(page => (page.style.display = "none"));
+
   //for dehighlighting inactive tabs
-  currenttab = document.getElementsByClassName('currentTab');
-  for(j = 0; j < currenttab.length; j++) {
-    currenttab[j].className = currenttab[j].className.replace("activetab","");
+  let currenttab = document.getElementsByClassName("currentTab");
+  for (j = 0; j < currenttab.length; j++) {
+    currenttab[j].className = currenttab[j].className.replace("activetab", "");
   }
   document.getElementById(highlighted).style.display = "flex";
   evnt.currentTarget.className += "activetab"; //this appends the color to active tab
